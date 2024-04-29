3532ea1a2f5d5df6e849ea86b0fa6f18271bd371
Juliette Peng <juliettepeng@nat-wireless-guest-reg-153-51.bu.edu>
2024-04-28 20:43:28 -0400

    More statistic functions added + one test function

--- project2/src/main.rs
+++ project2/src/main.rs
@@ -2,9 +2,8 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::collections::HashSet;
 use std::collections::HashMap;
-//use std::collections::VecDeque;
 mod stats;
-use stats::{mean_distance, bfs}; 
+use stats::{mean_distance, bfs, std_dev}; 
 
 fn read_file(path: &str) -> Vec<(usize, usize)> {
     let points = File::open(path).expect("Could not open file");
@@ -82,7 +81,51 @@ fn main() {
 
 let mean_dist = stats::mean_distance(&distances);
 println!("Mean Distance: {}", mean_dist);
+
+let sd = stats::std_dev(&distances, mean_dist);
+println!("Standard Deviation: {}", sd);
+
+test_bfs_zeros();
     
 }
-    
-//fn test_bfs 
+
+fn test_adjacency_list(graph: &Vec<(usize,usize)>, unique_nodes: HashSet<usize>) -> Vec<Vec<usize>> {
+    let new_num = unique_nodes.len();
+    let new_vec: Vec<&usize> = unique_nodes.iter().collect();
+    let mut adj: Vec<Vec<usize>> = vec![vec![];new_num];
+    let mut node_map: HashMap<usize, usize> = HashMap::new();
+    for (i, j) in new_vec.iter().enumerate(){
+        node_map.insert(**j,i);
+    }
+    for (source, destination) in graph {
+        adj[node_map[&source]].push(node_map[&destination]);
+    }
+    adj
+}
+
+fn test_bfs_zeros() -> i32{
+    let file = "test.txt";
+    let graph = read_file(file);
+    let unique_nodes = unique_nodes(&graph);
+    let test_adj = test_adjacency_list(&graph, unique_nodes.clone());
+
+    let mut passed = true;
+
+    for (i,node) in unique_nodes.iter().enumerate(){
+        let distances = bfs(&test_adj, i);
+        if distances[i] != Some(0) {
+            println!("Node {}: Expected 0, Got {:?}", node, distances[i]);
+            passed = false;
+        } else {
+            println!("Distance from node {}", i);
+            println!("To {}: {}", i, 0);
+        }
+     }
+     if passed {
+        println!("All nodes passed!");
+        0
+     }  else {
+        println!("Some nodes failed.");
+        1
+     }
+}
--- project2/src/stats.rs
+++ project2/src/stats.rs
@@ -36,3 +36,25 @@ pub fn mean_distance(distances: &[Vec<Option<usize>>]) -> f64 {
         0.0
     }
 }
+
+pub fn std_dev(distances: &[Vec<Option<usize>>], mean_distance: f64) -> f64 {
+    let mut sum_squared_diff = 0.0;
+    let mut total_nodes = 0;
+
+    for distance in distances {
+        for d in distance {
+            if let Some(dist) = d {
+                let diff = (*dist as f64 - mean_distance).powi(2);
+                sum_squared_diff += diff;
+                total_nodes += 1;
+            }
+        }
+    }
+
+    if total_nodes > 0 {
+        let var = sum_squared_diff / total_nodes as f64;
+        var.sqrt()
+    } else {
+        0.0
+    }
+}
